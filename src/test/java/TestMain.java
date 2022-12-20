2646e797a4d0551731026bc9810e43a17c6f16b1
RiptizeD <58150398+GlukhovVladimir@users.noreply.github.com>
2022-12-20 14:58:07 +0400

    Ggg

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -7,6 +7,6 @@ public class TestMain {
 
 @Test
 public void testInputIsEven(){
-assertTrue(Main.checkIfInputIsAnEvenNumber(21)); // Assertion
+assertTrue(Main.checkIfInputIsAnEvenNumber(22)); // Assertion
 }
 }
